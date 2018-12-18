e0c4f7885e67bc84e7a87f9d46e123a2f6580b08
oleksii_kuzminov <oleksii_kuzminov@epam.com>
2018-12-18 16:12:27 +0200

    initial project setup

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+## 0.0.1 2018-12-16
+ * Initial module setup
+ * Util's public interface added
---
+++ src/main/java/org/folio/isbn/IsbnUtil.java
@@ -0,0 +1,50 @@
+package org.folio.isbn;
+
+/**
+ * Class with utility methods for working with ISBN number
+ */
+public class IsbnUtil {
+
+  private IsbnUtil() {
+  }
+
+  /**
+   * Validate ten-digit ISBN
+   *
+   * @param number - ten-digit ISBN
+   * @return - is input number is valid for ten-digit ISBN format
+   */
+  public static boolean isValidTenDigitNumber(String number) {
+    return false;
+  }
+
+  /**
+   * Validate thirteen-digit ISBN
+   *
+   * @param number - thirteen-digit ISBN
+   * @return - is input number is valid for thirteen-digit ISBN format
+   */
+  public static boolean isValidThirteenDigitNumber(String number) {
+    return false;
+  }
+
+  /**
+   * Convert from ten-digit ISBN to the thirteen-digit ISBN
+   *
+   * @param number - valid ten-digit ISBN
+   * @return - thirteen-digit ISBN
+   */
+  public static String convertToThirteenDigitNumber(String number) {
+    return "";
+  }
+
+  /**
+   * Convert from thirteen-digit ISBN to the ten-digit ISBN
+   *
+   * @param number - valid thirteen-digit ISBN
+   * @return - ten-digit ISBN
+   */
+  public static String convertToTenDigitNumber(String number) {
+    return "";
+  }
+}
